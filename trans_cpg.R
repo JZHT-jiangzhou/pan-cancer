aa256c323692ccee2a1dc04dc947384d77b92ff9
JZHT-jiangzhou <85157561+JZHT-jiangzhou@users.noreply.github.com>
2021-06-01 16:28:04 +0800

    Delete trans_cpg.R

--- trans_cpg.R
+++
@@ -1,9 +0,0 @@
-#' @export
-#' @title transform cpg_site to gene
-#' @param cpg_site cpg_site list
-trans_cpg<-function(cpg_site)
-{
-  data(cpg)
-  sym = cpg[cpg_site,]$symbol
-  return(as.character(sym))
-}
